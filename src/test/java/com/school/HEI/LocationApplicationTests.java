fa757c5aa0dab4cd07a3a29ee5382accab51a8b8
NomenaFitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-11-27 10:08:39 +0300

    init project with a linter plugin

---
+++ src/test/java/com/school/HEI/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.HEI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
